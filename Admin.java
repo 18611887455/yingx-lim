98e2a957a1bab377e2a7dc0285ad8e1372d0ebff
lim <a18611887455@163.com>
2021-06-15 20:07:05 +0800

    添加一些代码V2.0

---
+++ Admin.java
@@ -0,0 +1,31 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="yx_admin")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+
+  @Id
+  private String id;
+
+  private String username;
+
+  private String password;
+
+  private String status;
+
+  private String salt;    //盐
+
+  private String level;   //级别
+
+
+
+}
